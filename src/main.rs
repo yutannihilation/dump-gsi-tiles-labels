de90c0b20efa00edc1ccbbe5f88f68fb4a73ae4b
Hiroaki Yutani <yutani.ini@gmail.com>
2025-03-30 01:38:03 +0900

    [WIP] dump texts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::error::Error;
 use std::io::Read as _;
+use std::{collections::HashMap, error::Error};
 
 use clap::{Parser, Subcommand};
 use directory::parse_directory;
@@ -40,6 +40,11 @@ enum Commands {
         length: usize,
         #[arg(long, default_value_t = 10)]
         limit: usize,
+    },
+    Text {
+        file: std::path::PathBuf,
+        #[arg(long)]
+        limit: Option<usize>,
     },
 }
 
@@ -165,6 +170,68 @@ fn dump_single_tile(
     Ok(())
 }
 
+fn dump_text(
+    file: &mut std::fs::File,
+    header: &PMTilesHeaderV3,
+    limit: Option<usize>,
+) -> Result<(), Box<dyn Error>> {
+    let root_dir_decoded = util::decompress(
+        file,
+        header.root_directory_offset,
+        header.root_directory_length as usize,
+        &header.internal_compression,
+    )?;
+
+    let (rest, entries) =
+        parse_directory(&root_dir_decoded).expect("Failed to parse root directory");
+    debug_assert!(rest.is_empty());
+
+    let mut result: HashMap<String, usize> = HashMap::new();
+
+    for e in &entries {
+        if e.is_tile {
+            let tile_decoded = util::decompress(
+                file,
+                header.tile_data_offset + e.offset,
+                e.length as usize,
+                &header.tile_compression,
+            )?;
+            let tile = mvt::Tile::decode(tile_decoded.as_slice())?;
+
+            for l in tile.layers {
+                for v in l.values {
+                    match v.string_value {
+                        Some(s) => {
+                            let count = result.entry(s).or_insert(0);
+                            *count += 1;
+                        }
+                        None => {}
+                    }
+                }
+            }
+        } else {
+            let leaf_dir_decoded = util::decompress(
+                file,
+                header.leaf_directories_offset + e.offset,
+                e.length as usize,
+                &header.internal_compression,
+            )?;
+
+            let (rest, leaf_entries) =
+                parse_directory(&leaf_dir_decoded).expect("Failed to parse leaf directory");
+            debug_assert!(rest.is_empty());
+
+            for le in &leaf_entries {
+                // TODO
+            }
+        }
+    }
+
+    println!("{result:#?}");
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
     let mut file = match &args.command {
@@ -172,6 +239,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Commands::ShowMetadata { file } => std::fs::File::open(file)?,
         Commands::List { file, .. } => std::fs::File::open(file)?,
         Commands::Tile { file, .. } => std::fs::File::open(file)?,
+        Commands::Text { file, .. } => std::fs::File::open(file)?,
     };
 
     // read header
@@ -191,6 +259,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             limit,
             ..
         } => dump_single_tile(&mut file, &header, *offset, *length, *limit)?,
+        Commands::Text { limit, .. } => dump_text(&mut file, &header, *limit)?,
     };
 
     Ok(())
